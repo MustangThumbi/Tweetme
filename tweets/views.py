0f9d3846114acbba492fc32e0cef837b11a09609
Vicky Thumbi <vickycrowny5@gmail.com>
2021-09-03 02:51:20 -0400

    initial Commit

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from .models import Tweet
 # Create your views here.
 
 
-def home_view(request, **args, **kwags):
+def home_view(request, *args_kwags):
     return HttpResponse("<h1>Hello</h1>")
+
+
+def tweet_details_view(request, tweet_id, *args_kwargs):
+    obj = Tweet.objects.get(id=tweet_id)
+    return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content}</h1>")
